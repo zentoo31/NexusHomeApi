b6a0b58d4b62a395b780b2a9b3b0e90fba2eaf4e
zentoo31 <zentoo31@gmail.com>
2025-10-01 13:27:48 -0500

    initial commit

---
+++ src/main/kotlin/com/zentoo/nexushomeapi/NexusHomeApiApplication.kt
@@ -0,0 +1,11 @@
+package com.zentoo.nexushomeapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NexusHomeApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<NexusHomeApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/zentoo/nexushomeapi/NexusHomeApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.zentoo.nexushomeapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NexusHomeApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
