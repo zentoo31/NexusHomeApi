edc18a3675d5e978f6ea89e40a0ea64574288956
zentoo31 <zentoo31@gmail.com>
2025-10-02 00:08:15 -0500

    add validation constraints to RegisterRequest data class

--- src/main/kotlin/com/zentoo/nexushomeapi/modules/auth/dto/RegisterRequest.kt
+++ src/main/kotlin/com/zentoo/nexushomeapi/modules/auth/dto/RegisterRequest.kt
@@ -1,8 +1,17 @@
 package com.zentoo.nexushomeapi.modules.auth.dto
 import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
 
 data class RegisterRequest(
     @field:Email(message = "El correo no es válido")
-    val email: String
+    @field:NotBlank(message = "El correo no puede estar vacío")
+    val email: String,
 
+    @field:NotBlank(message = "La contraseña no puede estar vacía")
+    @field:Size(min = 6, max = 100 , message = "La contraseña debe tener entre 6 y 100 caracteres")
+    val password: String,
+
+    @field:NotBlank(message = "El nombre no puede estar vacío")
+    val name: String
 )
